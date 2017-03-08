72517901f778e658fbd87b289257ebe0f310ee0b
Peter Trško <peter.trsko@gmail.com>
2017-03-08 22:52:19 +0100

    Correcting implementation of  fromOctetsToWord64
    Due to overflow of Word32 the high half of fromOctetsToWord64 result was
    discarded.

--- src/Data/Id/Utils.hs
+++ src/Data/Id/Utils.hs
@@ -53,6 +53,6 @@ fromOctetsToWord64 (o1, o2, o3, o4, o5, o6, o7, o8) =
     fromOctetsToWord64' (o1, o2, o3, o4) (o5, o6, o7, o8)
   where
     fromOctetsToWord64' hi lo =
-        fromIntegral (fromOctetsToWord32 hi `shiftL` 32)
+        (fromIntegral (fromOctetsToWord32 hi) `shiftL` 32)
         .|. fromIntegral (fromOctetsToWord32 lo)
 {-# INLINE fromOctetsToWord64 #-}
